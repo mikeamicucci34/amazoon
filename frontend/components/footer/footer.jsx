90a86f3b8490261254ac933ffa2533627473e539
Michael Amicucci <mikeamicucci34@gmail.com>
2021-09-09 10:59:34 -0400

    homepage stylying, footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 function Footer() {
     return (
         <div className="footer__component">
-            <p>This is the footer</p>
+            <div className="footer__componentText">
+                <p>Meet the creator</p>
+            </div >
+            <div className="footer__componentGithub">
+                <a href="https://github.com/mikeamicucci34">
+                    <GitHubIcon/>
+                </a>
+            </div>
+            <div className="footer__componentLinkedIn">
+                <a href="https://www.linkedin.com/in/michael-amicucci-428bb9122/">
+                    <LinkedInIcon/>
+                </a>
+            </div>
         </div>
     )
 }
